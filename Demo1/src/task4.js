301bdf7684b36d74376e7c474dd80d15dcb4e24c
AntonPanchenko <a.a.panchenkoo@gmail.com>
2020-02-23 19:06:54 +0200

    new version. dont work correctly

--- Demo1/src/task4.js
+++ Demo1/src/task4.js
@@ -1,23 +1,31 @@
-function checkPolindrome(num) {
+function checkPolindrome(str) {
     //Если строка содержит буквы, выдаем исключение
-    if(isNaN(Number(num))) return console.log(new Error('Аргумент не является числом. checkPolyndrom(Number)'));
-    if(num.length < 2) return console.log(new Error('Число не может быть меньше 10.'));
-    if(!Number.isInteger(+num)) return console.log(new Error('Число должно быть целым!'));
-    let search = finder(num);
-    if(search === '') return 'Полиндромы не найдены';
-    return search.split('').reverse().join('') + search;
+    if(isNaN(Number(str))) return console.log(new Error('Аргумент не является числом. checkPolyndrom(Number)'));
+    if(str.length < 2) return console.log(new Error('Число не может быть меньше 10.'));
+    if(!Number.isInteger(+str)) return console.log(new Error('Число должно быть целым!'));
+    let positions = [];
+    positions.push(finder(str));
+    console.log(positions)
 }
 
-//finder function
-function finder(num){
-    // const numLength = num.length;
-    let halfOfPolindrome = '';
-    for(let i = 0; i < num.length; i++){
-        if(num[i] === num[i + 1]){
-            halfOfPolindrome = num[i];
-            num = num.replace(num[i] + num[i+1], '');
-            halfOfPolindrome += finder(num);
+function finder(str){
+    const strLength = str.length;
+    let tempPosition;
+    let posittions = []
+    for(let i = 0; i < strLength; i++){
+        if(str[i]==str[i+1]){
+            for(let j = 0;j < strLength; j++){
+                if(str[i-j] == str[i+j+1]){
+                    tempPosition = [i-j,i+j+1];
+                } else{
+                    let findStr = str.split('').splice(tempPosition[0], tempPosition[1] - tempPosition[0] + 1).join('');
+                    str = str.replace(findStr, '');
+                    posittions.push(finder(str));
+                }
+            }
+            return tempPosition;
         }
     }
-    return halfOfPolindrome;
 }
+
+console.log(checkPolindrome('1725445213489980'))
