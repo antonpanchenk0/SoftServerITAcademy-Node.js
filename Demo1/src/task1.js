b8a46cc05980756de7dca2a3f0dc96f9e9e2bf46
laptop <>
2020-02-24 15:29:23 +0200

    Updating task1,2,3 in Demo #1

--- Demo1/src/task1.js
+++ Demo1/src/task1.js
@@ -1,19 +1,28 @@
+/**
+ *Функция для построения шамотной доски
+ * @param height высота доски
+ * @param width ширина доски
+ * @param char символ для доски
+ * @returns {string|{reason: string, status: string}}
+ */
 function createCheeseBoard(height, width, char) {
-    if(arguments.length != 3) return console.log({status: 'failed', reason: "Не верные параметры функции. createCheeseBoard(height, width, char)."});
-    if(width > 50 || width == 0 || height > 50 || height == 0) return console.log(new Error('Ошибка значений высоты и/или ширены. Допустимые значения от 1 до 50 включительно.'));
-    if(typeof height != "number" || typeof width != "number" || typeof char != "string") return console.log({status: 'failed', reason: "Не верные параметры функции. createCheeseBoard(height(type:Number), width(type:Number), char(type:String))."});
-    if(char == '') return console.log(new Error('Аргумент "Символ" не может быть пустым!'));
+    if(arguments.length != 3) return {status: 'failed', reason: "Не верные параметры функции. createCheeseBoard(height, width, char)."};
+    if(width > 50 || width == 0 || height > 50 || height == 0) return {status: 'failed', reason: "Ошибка значений высоты и/или ширены. Допустимые значения от 1 до 50 включительно."};
+    if(typeof height != "number" || typeof width != "number" || typeof char != "string") return {status: 'failed', reason: "Не верные параметры функции. createCheeseBoard(height(type:Number), width(type:Number), char(type:String))."};
+    if(char == '') return {status: "failed", reason: "Аргумент char не может біть пустым"};
     let h = Math.abs(Math.floor(height));
     let w = Math.abs(Math.floor(width));
     let c = char[0];
+    let string = '';
     for(let i = 0; i < h; i++){
         for(let j =0; j < w; j++){
             if(i%2) {
-                cheese.innerHTML += j % 2 ? ' ' : c;
+                string += j % 2 ? ' ' : c;
             }else{
-                cheese.innerHTML += j % 2 ? c : ' ';
+                string += j % 2 ? c : ' ';
             }
         }
-        cheese.innerHTML += '\n';
+        string += '\n';
     };
+    return string;
 }
--- Demo1/src/task2.js
+++ Demo1/src/task2.js
@@ -1,7 +1,18 @@
+/**
+ * Функция опрделяет можно ли вложить один прямоугольник в другой
+ * @param rectangle_1: object{h: number, w: number}
+ * @param rectangle_2: object{h: number, w: number}
+ * @returns {{reason: string, status: string}|number}
+ */
 function rectangleInRectangle(rectangle_1, rectangle_2) {
-    if(typeof rectangle_1 != "object" || typeof rectangle_2 != "object") return console.log({status: 'failed', reason: "Не верные параметры функции. rectangleInRectangle(rectangle_1(type: object), rectangle_2(type: object)"});
-    if(rectangle_1['h'] == undefined || rectangle_2['h'] == undefined || rectangle_1['w'] == undefined || rectangle_2['w'] == undefined) return console.log(new Error('Неверные ключи у объекта - параметра функции. obj{ w: Number, h: Number }.'));
-    if(typeof rectangle_1['h'] != "number" || typeof rectangle_2['h'] != "number" || typeof rectangle_1['w'] != "number" || typeof rectangle_2['w'] != "number") return console.log(new Error('Неверные ключи у объекта - параметра функции. obj{ w: Number, h: Number }.'));
+    //Проверка на то, являеется ли переданные параметры объектами
+    if(typeof rectangle_1 != "object" || typeof rectangle_2 != "object") return {status: 'failed', reason: "Не верные параметры функции. rectangleInRectangle(rectangle_1(type: object), rectangle_2(type: object)"};
+    //Проверка все ли данные о конвертах получены (ширина и высота)
+    if(rectangle_1['h'] == undefined || rectangle_2['h'] == undefined || rectangle_1['w'] == undefined || rectangle_2['w'] == undefined) return {status: "failed", reason: 'Неверные ключи у объекта - параметра функции. obj{ w: Number, h: Number }.'};
+    //Если переданы не числа в качестве параметрой объекта
+    if(typeof rectangle_1['h'] != "number" || typeof rectangle_2['h'] != "number" || typeof rectangle_1['w'] != "number" || typeof rectangle_2['w'] != "number") return {status: 'failed', reason: 'Неверные ключи у объекта - параметра функции. obj{ w: Number, h: Number }.'};
+    //Если передано больше чем 2 аргумента
+    if(arguments.length > 2) return {status: 'failed', reason: 'Большое количество аргументов.'};
     let big = null;
     let small = null;
     let numberOfBig = null;
@@ -15,11 +26,21 @@ function rectangleInRectangle(rectangle_1, rectangle_2) {
         small = rectangle_1;
         numberOfBig = 2;
     };
+    /**Условие проверяет по формуле сможет ли меньший конверт вложится в больший под каким-либо уголом
+     * Формула для проверки:
+     * Small - w: ширина, h: высота
+     * Big - w: ширина, h: высота
+     * Вложение возможно если:
+     * 1)small.w <= big.w и small.h <= big.h
+     * или
+     * 2)small.w > big.w и big.h >= (2*small.w*small.h + (big.w^2 - big.h^2)*Корень(big.w^2+big.h^2 - small.w^2)) / big.w^2 + big.h^2
+     */
     if((small.w <= big.w && small.h <= big.h)
         ||
-        (small.w > big.w && big.h >= (2*small.w*small.h*big.w + (big.w*big.w - big.h*big.h)*Math.sqrt(big.w*big.w+big.h*big.h-small.w*small.w))/(big.w*big.w+big.h*big.h)))
+        (small.w > big.w && big.h >= (2*small.w*small.h*big.w + (Math.pow(big.w, 2) - Math.pow(big.h, 2))*Math.sqrt(Math.pow(big.w,2)+Math.pow(big.h,2)-Math.pow(small.w,2)))/(Math.pow(big.w,2)+Math.pow(big.h,2))))
     {
         return numberOfBig;
     }
-    else return 0;
+    else
+        return 0;
 }
